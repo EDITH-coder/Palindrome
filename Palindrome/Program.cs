1a1708c026c8b801ce61085563624ae6d2905c8d
EDITH_Coder <edithmashapo@gmail.com>
2023-10-16 04:32:09 +0200

    Add project files.

---
+++ Palindrome/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaration and Initialization
+            string Word, reversedWord = "";
+            Console.Write(" Enter a Word: ");
+            Word = Console.ReadLine();
+            //String Reverse  
+            for (int i = Word.Length - 1; i >= 0; i--) 
+            {
+                reversedWord += Word[i].ToString();
+            }
+            // Checking whether string is palindrome or not  
+            if (reversedWord == Word)
+            {
+                
+                Console.WriteLine("A Word is Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("A Word is not Palindrome");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
